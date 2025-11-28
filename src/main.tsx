d398b1ec01e69597a3b335b20e574895b8a5d484
ics-nishihara <nishihara@ics-web.jp>
2025-11-28 19:54:59 +0900

    ルーターの調整

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
       <App />
     </BrowserRouter>
   </StrictMode>
